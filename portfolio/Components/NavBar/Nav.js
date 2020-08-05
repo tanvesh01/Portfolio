5a9dcc21d5b6fa0ecef9ffde59efb250af3db021
tanvesh <sarvetanvesh01@gmail.com>
2020-08-05 12:51:05 +0530

    added image?

--- portfolio/Components/NavBar/Nav.js
+++ portfolio/Components/NavBar/Nav.js
@@ -1,42 +1,6 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles({
-    navLinks: {
-        listStyle: "none",
-        display: "flex",
-    },
-    nav: {
-        position: "fixed",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        width: "100%",
-        height: "4rem",
-        fontFamily: "'Haas'",
-        top: 0,
-        fontSize: "16px",
-        zIndex: 1,
-        paddingLeft: "30px",
-        paddingRight: "10px",
-        transition: "background-color 0.6s",
-    },
-    tag: {
-        display: "inline-block",
-        textDecoration: "none",
-        color: "black",
-        padding: "12px",
-        "&:hover": {
-            color: "#00ff00",
-        },
-    },
-    brand: {
-        fontWeight: 800,
-    },
-});
-
+import classes from "./NavBar.module.css";
 const Nav = (props) => {
-    const classes = useStyles();
     return (
         <nav
             className={classes.nav}
--- portfolio/Containers/Intro/Intro.js
+++ portfolio/Containers/Intro/Intro.js
@@ -4,7 +4,7 @@ import classes from "./Intro.module.css";
 const Intro = (props) => {
     return (
         <div className={classes.hi}>
-            <img src="/images/green.svg" alt="green" className={classes.green} />
+            <img src="/green.svg" alt="green" className={classes.green} />
             <h1 className={classes.heading}>
                 Hey! I’m Tanvesh, a India-Based Full-Stack Developer.
             </h1>
