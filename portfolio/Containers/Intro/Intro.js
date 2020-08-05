31d61fb194f6cf95cfd5ef50682f9694f3307563
tanvesh <sarvetanvesh01@gmail.com>
2020-08-05 12:38:24 +0530

    added css modules

--- portfolio/Containers/Intro/Intro.js
+++ portfolio/Containers/Intro/Intro.js
@@ -1,51 +1,7 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles({
-    hi: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        position: "relative",
-        width: "100%",
-        height: "22rem",
-        // border: "2px solid #00ff00",
-        marginTop: "9rem",
-        fontFamily: "'Haas'",
-    },
-    heading: {
-        fontSize: "60px",
-        width: "52rem",
-        fontWeight: "300",
-        zIndex: 0,
-    },
-    green: {
-        height: "29rem",
-        position: "absolute",
-        // opacity: 0.8,
-        marginLeft: "-26%",
-        marginTop: "-3rem",
-    },
-    "@media only screen and (max-width: 600px)": {
-        green: {
-            display: "none",
-        },
-        heading: {
-            fontSize: "11.5vw",
-            textAlign: "left",
-            lineHeight: "13vw",
-            marginLeft: "2rem",
-        },
-        hi: {
-            width: "88%",
-            marginTop: 0,
-            height: "27rem",
-        },
-    },
-});
+import classes from "./Intro.module.css";
 
 const Intro = (props) => {
-    const classes = useStyles();
     return (
         <div className={classes.hi}>
             <img src="/images/green.svg" alt="green" className={classes.green} />
