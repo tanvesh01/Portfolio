18fc813fee892752df28cc420532a5da1148372e
tanvesh01 <sarvetanvesh01@gmail.com>
2021-11-07 22:44:57 +0530

    added even more resume redirect

---
+++ next.config.js
@@ -0,0 +1,23 @@
+module.exports.redirects = async function () {
+    return [
+        {
+            source: "/resume",
+            destination:
+                "https://serious-palm-014.notion.site/tanvesh-s-resume-866cbe37f9524232b6e75600f474891d",
+            permanent: true,
+        },
+        {
+            source: "/cv",
+            destination:
+                "https://serious-palm-014.notion.site/tanvesh-s-resume-866cbe37f9524232b6e75600f474891d",
+            permanent: true,
+        },
+        // this is a joke
+        {
+            source: "/rashi",
+            destination:
+                "https://serious-palm-014.notion.site/tanvesh-s-resume-866cbe37f9524232b6e75600f474891d",
+            permanent: true,
+        },
+    ];
+};
