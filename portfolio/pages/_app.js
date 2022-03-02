ad90572b45e0f45799c32b512d68a66789228684
tanvesh01 <sarvetanvesh01@gmail.com>
2022-03-02 15:09:30 +0530

    chore: even more meta changes

--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -28,7 +28,6 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <meta name="twitter:image" content="/meta.png"></meta>
         <meta name="twitter:card" content="/meta.png"></meta>
         <meta name="twitter:creator" content={twitterHandle} key="twhandle" />
 
@@ -43,6 +42,7 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:site_name" content={"Tanvesh"} key="ogsitename" />
         {/* Twitter */}
         {/* <meta name="twitter:card" content="summary" key="twcard" /> */}
+        {/* <meta name="twitter:image" content="/meta.png"></meta> */}
 
         <title>Tanvesh</title>
       </Head>
