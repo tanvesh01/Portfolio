5b6038a689a19f94d9f50eaae6e7eb286a76456a
tanvesh01 <sarvetanvesh01@gmail.com>
2022-03-02 15:54:44 +0530

    chore: added summary_large_image tag

--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -28,7 +28,8 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <meta name="twitter:card" content="/meta.png"></meta>
+        <meta name="twitter:image:src" content="/meta.png"></meta>
+        <meta name="twitter:card" content="summary_large_image"></meta>
         <meta name="twitter:creator" content={twitterHandle} key="twhandle" />
 
         <meta name="theme-color" content="#1de400"></meta>
