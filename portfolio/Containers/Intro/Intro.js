7040e454ec4b246a5dc767bb1aec8ccc792f7193
tanvesh01 <sarvetanvesh01@gmail.com>
2022-03-02 14:20:52 +0530

    added meta tags and image

--- portfolio/Containers/Intro/Intro.js
+++ portfolio/Containers/Intro/Intro.js
@@ -2,14 +2,14 @@ import React from "react";
 import classes from "./Intro.module.css";
 
 const Intro = (props) => {
-    return (
-        <div className={classes.hi}>
-            <img src="/green.svg" alt="green" className={classes.green} />
-            <h1 className={classes.heading}>
-                Hey! I’m Tanvesh, a India-Based Full-Stack Developer.
-            </h1>
-        </div>
-    );
+  return (
+    <div className={classes.hi}>
+      <img src="/green.svg" alt="green" className={classes.green} />
+      <h1 className={classes.heading}>
+        Hey! I’m Tanvesh, a India-Based Frontend Engineer.
+      </h1>
+    </div>
+  );
 };
 
 export default Intro;
--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -2,26 +2,52 @@ import "../styles/globals.css";
 import "../public/Projects.css";
 import "../public/projectGallery.css";
 import { motion } from "framer-motion";
+import Head from "next/head";
 
 const apps = {
-    initial: {
-        opacity: 0,
+  initial: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: "easeIn",
     },
-    visible: {
-        opacity: 1,
-        transition: {
-            duration: 1,
-            ease: "easeIn",
-        },
-    },
+  },
 };
 
+const DESCRIPTION =
+  "I build apps with beautiful UI/UX. Doing what I absolutely love.";
+const twitterHandle = "@Sarve___tanvesh";
+const currentURL = "https://tanvesh.vercel.app/";
+
 function MyApp({ Component, pageProps }) {
-    return (
-        <motion.div initial="initial" animate="visible" variants={apps}>
-            <Component {...pageProps} />
-        </motion.div>
-    );
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="twitter:image" content="/meta.png"></meta>
+        <meta name="twitter:creator" content={twitterHandle} key="twhandle" />
+
+        <meta name="description" content={DESCRIPTION}></meta>
+        <meta property="og:title" content={"Tanvesh"} key="ogtitle" />
+        <meta property="og:description" content={DESCRIPTION} key="ogdesc" />
+        <meta property="og:image" content={"/meta.png"} key="ogimage" />
+        <meta property="og:url" content={currentURL} key="ogurl" />
+        {/* Open Graph */}
+        <meta property="og:site_name" content={"Tanvesh"} key="ogsitename" />
+        {/* Twitter */}
+        {/* <meta name="twitter:card" content="summary" key="twcard" /> */}
+
+        <title>Tanvesh</title>
+      </Head>
+      <motion.div initial="initial" animate="visible" variants={apps}>
+        <Component {...pageProps} />
+      </motion.div>
+    </>
+  );
 }
 
 export default MyApp;
