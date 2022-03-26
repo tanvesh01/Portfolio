c7c8d4a55ea239bc00d581aa8966192fc7ee7843
tanvesh01 <sarvetanvesh01@gmail.com>
2022-03-26 17:46:22 +0530

    added graph paper

--- portfolio/src/GalleryComponents/LandscapeGrid/LandscapeGrid.tsx
+++ portfolio/src/GalleryComponents/LandscapeGrid/LandscapeGrid.tsx
@@ -1,6 +1,7 @@
 import { keyframes, styled } from "@styles/stitches";
 import React from "react";
 import { resultFromUnsplash } from "src/constants";
+import { StyledLinksContainer } from "./components";
 
 const move = keyframes({
   "0%": { transform: "rotate(0deg)" },
@@ -13,6 +14,7 @@ const LandscapeGrid = () => {
       {resultFromUnsplash.results.map((info, index) => {
         return (
           <CardContainer>
+            <Texture />
             <BigImage src={info.urls.regular} />
             <SmallImage src={info.urls.regular} />
             <SmallImageWrapper></SmallImageWrapper>
@@ -21,9 +23,11 @@ const LandscapeGrid = () => {
               <StyledHeading>
                 {info.description || info.alt_description}
               </StyledHeading>
-              <StyledLinkAuthor href={info.user.portfolio_url}>
-                {info.user.name}
-              </StyledLinkAuthor>
+              <StyledLinksContainer>
+                <StyledLinkAuthor href={info.user.portfolio_url}>
+                  {info.user.name}
+                </StyledLinkAuthor>
+              </StyledLinksContainer>
             </div>
           </CardContainer>
         );
@@ -32,8 +36,30 @@ const LandscapeGrid = () => {
   );
 };
 
+const Texture = styled("div", {
+  backgroundColor: "#DFDBE5",
+  backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%239C92AC' fill-opacity='0.4'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  zIndex: 6,
+  opacity: "0.3",
+  mixBlendMode: "color-dodge",
+});
+
 const StyledLinkAuthor = styled("a", {
-  color: "grey",
+  color: "#e7e6e6",
+  "&:hover": {
+    color: "white",
+  },
+  "&::after": {
+    content: "",
+    display: "block",
+    width: "100%",
+    borderTop: "1px dashed white",
+  },
 });
 
 const StyledHeading = styled("h4", {
@@ -67,17 +93,17 @@ const SmallImage = styled("img", {
   left: "0%",
   zIndex: -1,
   filter: "blur(30px)",
-  animation: `${move} 10s infinite linear`,
+  animation: `${move} 40s infinite linear`,
   // clipPath: "polygon(50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%)",
 });
 
 const ImagePlaceholder = styled("img", {
   width: "100%",
-  height: "40%",
+  height: "auto",
   backgroundColor: "white",
   // margin: "0 auto",
   position: "relative",
-  zIndex: "3",
+  zIndex: "6",
   display: "block",
 });
 
---
+++ portfolio/src/GalleryComponents/LandscapeGrid/components.tsx
@@ -0,0 +1,5 @@
+import { styled } from "@styles/stitches";
+
+export const StyledLinksContainer = styled("div", {
+  width: "fit-content",
+});
